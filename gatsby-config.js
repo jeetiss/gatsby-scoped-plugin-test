c4a05e0f71c2b39e77db0d6d2dc0b88adb069bde
jeetiss <jeetiss@yandex.ru>
2019-07-04 10:57:51 +0300

    use scoped plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `@jeetiss/gatsby-scoped-plugin`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
